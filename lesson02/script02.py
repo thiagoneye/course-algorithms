b30a0173a4d7484097687f4fc00bad3beb2e7c41
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2022-12-30 02:52:37 -0300

    Update ./lesson02/script02.py file

--- lesson02/script02.py
+++ lesson02/script02.py
@@ -11,9 +11,45 @@ class TreeNode:
         self.right = None
 
 
+# Functions
+
+def parse_tuple(data):
+    if isinstance(data, tuple) and len(data) == 3:
+        node = TreeNode(data[1])
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+    elif data is None:
+        node = None
+    else:
+        node = TreeNode(data)
+
+    return node
+
+def tree_to_tuple(tree):
+    if tree is None:
+        return None
+    return (tree_to_tuple(tree.left), tree.key, tree_to_tuple(tree.right))
+
+def display_keys(node, space='\t', level=0):
+    if node is None:
+        print(space*level + '∅')
+        return
+
+    if node.left is None and node.right is None:
+        print(space*level + str(node.key))
+        return
+
+    display_keys(node.right, space, level+1)
+    print(space*level + str(node.key))
+    display_keys(node.left, space, level+1)
+
+
 # Main
 
 if __name__ == '__main__':
-    node0 = TreeNode(3)
-    node1 = TreeNode(4)
-    node2 = TreeNode(5)
+    tree_tuple = ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
+    tree = parse_tuple(tree_tuple)
+
+    display_keys(tree)
+
+    print(tree_to_tuple(tree))
