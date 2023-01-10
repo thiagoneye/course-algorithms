8e89ad873b40f7f0c128e61ce11613187b0ee8b6
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2023-01-09 22:23:58 -0300

    Update ./04_01-lesson/script01.py file

--- 04_01-lesson/README.md
+++ 04_01-lesson/README.md
@@ -1,7 +1,11 @@
 # Problem
 
+Write a function to find the length of the longest common subsequence between two sequences. E.g. Given the strings "serendipitous" and "precipitation", the longest common subsequence is "reipito" and its length is 7.
 
+A "sequence" is a group of items with a deterministic ordering. Lists, tuples and ranges are some common sequence types in Python.
+
+A "subsequence" is a sequence obtained by deleting zero or more elements from another sequence. For example, "edpt" is a subsequence of "serendipitous".
 
 # List of Scripts
 
-1.
+1. Longest Common Subsequence (Recursive)
--- 04_01-lesson/script01.py
+++ 04_01-lesson/script01.py
@@ -1,16 +1,25 @@
 """
-
+Longest Common Subsequence
 """
-
-# Classes
-
-
 
 # Functions
 
+def lcs_recursive(seq1: str, seq2: str, idx1=0, idx2=0) -> str:
+    if idx1 == len(seq1) or idx2 == len(seq2):
+        return 0
+    elif seq1[idx1] == seq2[idx2]:
+        return 1 + lcs_recursive(seq1, seq2, idx1+1, idx2+1)
+    else:
+        option1 = lcs_recursive(seq1, seq2, idx1+1, idx2)
+        option2 = lcs_recursive(seq1, seq2, idx1, idx2+1)
+
+        return max(option1, option2)
 
 
 # Main
 
 if __name__ == '__main__':
-    pass
+    seq1 = 'serendipitous'
+    seq2 = 'precipitation'
+    seq = lcs_recursive(seq1, seq2)
+    print(seq)
