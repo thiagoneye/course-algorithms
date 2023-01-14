9b12783a64bef23c821f1b0fffc0ea715bfd137d
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2023-01-14 00:36:21 -0300

    Started the folder ./05_01-lesson/

--- 05_01-lesson/script01.py
+++ 05_01-lesson/script01.py
@@ -1,16 +1,30 @@
 """
-
+1. Create a class to represent a graph as an adjacency list in Python.
 """
 
 # Classes
 
+class Graph:
+    def __init__(self, num_nodes, edges):
+        self.num_nodes = num_nodes
+        self.data = [[] for _ in range(num_nodes)]
+        for n1, n2 in edges:
+            self.data[n1].append(n2)
+            self.data[n2].append(n1)
 
-
-# Functions
-
+    def __repr__(self):
+        [enumerate(self.data)]
 
 
 # Main
 
 if __name__ == '__main__':
-    pass
+    num_nodes = 5
+    edges = [(0, 1), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4)]
+
+    graph = Graph(num_nodes, edges)
+
+    print(graph.data)
+
+    for value in enumerate(graph.data):
+        print(value)
