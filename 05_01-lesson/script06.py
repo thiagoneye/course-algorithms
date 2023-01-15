ea96d025eed0f33fdb444b4af0495132e7885ba3
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2023-01-15 11:33:57 -0300

    Update README.md and script06.py files

--- 05_01-lesson/script06.py
+++ 05_01-lesson/script06.py
@@ -1,13 +1,72 @@
 """
-Shortest Paths
+Shortest Path (Dijkstra's Algorithm)
 """
+
+# Imports
+
+from script05 import Graph
+
 
 # Functions
 
+def update_distances(graph, current, distance, parent=None):
+    """
+    Update the distances of the current node's neighbors.
+    """
+    neighbors = graph.data[current]
+    weights = graph.weight[current]
 
+    for i, node in enumerate(neighbors):
+        weight = weights[i]
+        if distance[current] + weight < distance[node]:
+            distance[node] = distance[current] + weight
+            if parent:
+                parent[node] = current
+
+
+def pick_next_node(distance, visited):
+    """
+    Pick the next unvisited node at the smallest distance.
+    """
+    min_distance = float('inf')
+    min_node = None
+
+    for node in range(len(distance)):
+        if not visited[node] and distance[node] < min_distance:
+            min_distance = distance[node]
+            min_node = node
+
+    return min_node
+
+
+def shortest_path(graph, source, target):
+    visited = [False] * graph.num_nodes
+    distance = [float('inf')] * graph.num_nodes
+    queue = []
+
+    distance[source] = 0
+    queue.append(source)
+    idx = 0
+
+    while idx < len(queue) and not visited[target]:
+        current = queue[idx]
+        visited[current] = True
+        idx += 1
+
+        update_distances(graph, current, distance)
+        next_node = pick_next_node(distance, visited)
+
+        if next_node:
+            queue.append(next_node)
+
+    return distance[target]
 
 
 # Main
 
 if __name__ == '__main__':
-    pass
+    num_nodes = 6
+    edges = [(0, 1, 4), (0, 2, 2), (1, 2, 5), (1, 3, 10), (2, 4, 3), (4, 3, 4), (3, 5, 11)]
+    graph = Graph(num_nodes, edges, weighted=True, directed=True)
+
+    print(shortest_path(graph, 0, 5))
